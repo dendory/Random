1780bdd7e75a006f2425e23f3009bb124f8f82b5
Patrick Lambert <dendory@live.ca>
2014-10-04 11:21:32 -0400

    Initial code

---
+++ Random/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace RandomNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chars = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.min)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(331, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "                                                                                 " +
+                "                     ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 37);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(331, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "                                                                                 " +
+                "                     ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Random number between";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // min
+            // 
+            this.min.Location = new System.Drawing.Point(169, 11);
+            this.min.Maximum = new decimal(new int[] {
+            99998,
+            0,
+            0,
+            0});
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(55, 20);
+            this.min.TabIndex = 3;
+            this.min.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "and";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // max
+            // 
+            this.max.Location = new System.Drawing.Point(263, 11);
+            this.max.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(55, 20);
+            this.max.TabIndex = 5;
+            this.max.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Random string of";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // chars
+            // 
+            this.chars.Location = new System.Drawing.Point(126, 35);
+            this.chars.Maximum = new decimal(new int[] {
+            64,
+            0,
+            0,
+            0});
+            this.chars.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.chars.Name = "chars";
+            this.chars.Size = new System.Drawing.Size(55, 20);
+            this.chars.TabIndex = 7;
+            this.chars.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "characters";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(12, 75);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(438, 29);
+            this.result.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 119);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chars);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random";
+            ((System.ComponentModel.ISupportInitialize)(this.min)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown min;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown max;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown chars;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox result;
+    }
+}
+
---
+++ Random/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Select();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Select();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            radioButton2.Select();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            radioButton2.Select();
+        }
+
+        private String Number2String(int number, bool isCaps)
+        {
+
+            Char c = (Char)((isCaps ? 65 : 97) + (number - 1));
+            return c.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            if (radioButton1.Checked == true)
+            {
+                if (min.Value < max.Value)
+                {
+                    int randomNumber = random.Next(Convert.ToInt32(min.Value), Convert.ToInt32(max.Value)+1);
+                    result.Text = randomNumber.ToString();
+                }
+                else result.Text = "Min must be lesser than Max.";
+            }
+            else
+            {
+                string str = "";
+                for (int c = 1; c < chars.Value+1; c++)
+                {
+                    int randomNumber = random.Next(0, 61);
+                    if (randomNumber > 9 && randomNumber < 36) str += Number2String(randomNumber - 9, false);
+                    else if (randomNumber > 35) str += Number2String(randomNumber - 35, true);
+                    else str += randomNumber.ToString();
+                }
+                result.Text = str;
+            }
+        }
+    }
+}
